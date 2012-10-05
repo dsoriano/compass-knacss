213953c0c2de053bca78007a33aa5d6572f8f306
teknipc <infos@teknipc.com>
2012-10-05 22:41:52 +0200

    Initialisation du projet

---
+++ lib/knacss.rb
@@ -0,0 +1,2 @@
+require 'compass'
+Compass::Frameworks.register("knacss", :path => "#{File.dirname(__FILE__)}/..")
---
+++ templates/project/manifest.rb
@@ -0,0 +1,20 @@
+stylesheet 'style.scss', :media => "screen, projection"
+stylesheet 'inc/_footer.scss'
+stylesheet 'inc/_forms.scss'
+stylesheet 'inc/_header.scss'
+stylesheet 'inc/_main.scss'
+stylesheet 'inc/_nav.scss'
+stylesheet 'inc/_print.scss'
+stylesheet 'inc/_sidebar.scss'
+stylesheet 'inc/_tables.scss'
+
+description "KNACSS Compass Extension v2.0"
+
+help %Q{
+https://github.com/teknipc/compass-knacss
+}
+
+welcome_message %Q{
+https://github.com/teknipc/compass-knacss
+}
+
